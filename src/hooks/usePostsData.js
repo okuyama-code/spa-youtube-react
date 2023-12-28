95ac4e761e3d76a4e2d557b55aed9de895dfa39c
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-28 15:54:46 +0900

    途中まで動作確認

--- src/hooks/usePostsData.js
+++ src/hooks/usePostsData.js
@@ -1,21 +1,27 @@
 import { useState, useEffect } from "react";
 import { fetchAllPosts, searchPosts } from "../services/postService";
 
-function usePostsData(searchTerm) {
+function usePostsData(searchTerm, page = 1) {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [perPage, setPerPage] = useState(10);
 
   useEffect(() => {
     async function loadPosts() {
       try {
         let data;
         if (searchTerm) {
-          data = await searchPosts(searchTerm);
+          data = await searchPosts(searchTerm, page);
         } else {
-          data = await fetchAllPosts();
+          data = await fetchAllPosts(page);
         }
-        setPosts(data);
+        if (data.posts) {
+          setPosts(data.posts);
+          setTotalPosts(data.total_count)
+          setPerPage(data.per_page)
+        }
         setLoading(false);
       } catch (e) {
         setError(e);
@@ -26,7 +32,7 @@ function usePostsData(searchTerm) {
     loadPosts();
   }, [searchTerm]);
 
-  return { posts, loading, error };
+  return { posts, loading, error, totalPosts, perPage };
 }
 
 export default usePostsData;
