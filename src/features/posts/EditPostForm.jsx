95b6577165e6350459c9918e9bf1648fe3b72fe0
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-29 04:45:49 +0900

    修正

--- src/features/posts/EditPostForm.jsx
+++ src/features/posts/EditPostForm.jsx
@@ -30,18 +30,18 @@ const EditPostForm = () => {
 
   // "rawData" は生の、変更や処理を加えていないデータを表します。
   const handleUpdateSubmit = async (rawData) => {
-    const formData = new FormData();
-    formData.append("post[title]", rawData.title)
-    formData.append("post[body]", rawData.body)
-    formData.append("post[image]", rawData.image)
+    // const formData = new FormData();
+    // formData.append("post[title]", rawData.title)
+    // formData.append("post[body]", rawData.body)
+    // formData.append("post[image]", rawData.image)
 
     // "sanitizedData" は、「無害化されたデータ」や「安全なデータ」といった意味を持ちます。この用語は通常、セキュリティやデータ整合性の観点から、悪意のあるデータや不正確なデータから除外され、信頼性を確保されたデータを指します。
-    // const sanitizedData = {
-    //   title: rawData.title,
-    //   body: rawData.body,
-    //   image: rawData.image,
-    // }
-    // const formData = objectToFormData({ post: sanitizedData })
+    const sanitizedData = {
+      title: rawData.title,
+      body: rawData.body,
+      image: rawData.image,
+    }
+    const formData = objectToFormData({ post: sanitizedData })
     try {
       const response = await updatePost(id, formData)
       navigate(`/posts/${response.id}`)
